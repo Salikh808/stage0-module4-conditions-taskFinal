40a8cf1b0893a0f9453ffba2f3509f17d10101ce
nurzhan <Salikh835@gmail.com>
2023-02-16 22:45:23 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,20 +2,21 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        if (year == 366) {
-            switch (month) {
-                case 1, 3, 7, 8, 10, 12 -> System.out.println(31);
-                case 11, 9, 6, 4 -> System.out.println(30);
-                case 2 -> System.out.println(29);
-            }
+        switch (month) {
+            case 1, 3, 7, 8, 10, 12 -> System.out.println(31);
+            case 11, 9, 6, 4 -> System.out.println(30);
+            default -> System.out.println("invalid date");
         }
-        if (year == 365) {
-            switch (month) {
-                case 1, 3, 7, 8, 10, 12 -> System.out.println(31);
-                case 11, 9, 6, 4 -> System.out.println(30);
-                case 2 -> System.out.println(28);
+
+        if (year == 366) {
+            if (month == 2) {
+                System.out.println(29);
             }
-        }else {
+        } else if (year == 365) {
+            if (month == 2) {
+                System.out.println(28);
+            }
+        } else {
             System.out.println("invalid date");
         }
     }
